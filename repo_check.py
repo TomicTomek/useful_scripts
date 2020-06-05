4b1dd796041dfc0cf057e26471fec365037ea109
Tomek <TomicTomek@enplug.com>
2020-06-05 10:43:09 +0200

    update repo_check script

--- repo_check.py
+++ repo_check.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from git import Repo
 import xml.etree.ElementTree as ET
-from sys import exit
+import sys
+
 
 
 class bcolors:
@@ -40,13 +41,13 @@ def is_repo_dirty(repo_path):
     repo = Repo(repo_path)
     return repo.is_dirty(index=True, working_tree=True, untracked_files=True)
 
-    
+
 def print_untracked_files(repo_path):
     repo = Repo(repo_path)
     has_untracked = repo.is_dirty(index=False, working_tree=False, untracked_files=True)
     if has_untracked:
         for untracked_file_path in repo.untracked_files:
-            print(bcolors.YELLOW + "\t" +untracked_file_path + bcolors.ENDC)
+            print(bcolors.YELLOW + "\t[u] " + untracked_file_path + bcolors.ENDC)
 
 
 def print_modified_files(repo_path):
@@ -54,7 +55,7 @@ def print_modified_files(repo_path):
     has_modified = repo.is_dirty(index=False, working_tree=True, untracked_files=False)
     if has_modified:
         for modified_file_path in repo.index.diff(None):
-            print(bcolors.RED, "\t", modified_file_path.a_path, bcolors.ENDC)
+            print(bcolors.RED + "\t[m] " + modified_file_path.a_path + bcolors.ENDC)
 
 
 def print_stagged_files(repo_path):
@@ -62,8 +63,74 @@ def print_stagged_files(repo_path):
     has_indexed = repo.is_dirty(index=True, working_tree=False, untracked_files=False)
     if has_indexed:
         for indexed_file_path in repo.index.diff(repo.head.commit):
-            print(bcolors.BLUE, "\t", indexed_file_path.a_path, bcolors.ENDC)
+            print(bcolors.BLUE + "\t[i] " + indexed_file_path.a_path + bcolors.ENDC)
 
+# remote compare START
+def is_branch_present_on_remote(remote, branch_name):
+    is_present = False
+    try:
+        remote.refs[branch_name]
+        is_present = True
+    except (AttributeError, IndexError):
+        is_present = False
+    return is_present
+
+
+def local_and_remote_are_at_same_commit(repo, remote_name, branch_name, local_hexsha=None):
+    if branch_name == None and local_hexsha == None:
+        raise InputError("branch_name and commit_hexsha are None")
+
+    remote = repo.remotes[remote_name]
+
+    if branch_name == None:
+        # iterate by all branches
+        for remote_branch_ref in remote.refs:
+            if local_hexsha == remote_branch_ref.commit.hexsha:
+                return True
+        return False
+
+    if local_hexsha == None:
+        local_commit = repo.heads[branch_name].commit
+        local_hexsha = local_commit.hexsha
+
+    remote_commit = remote.refs[branch_name].commit
+    return local_hexsha == remote_commit.hexsha
+
+
+def is_remote_and_local_same(repo_project, force_fetch=False):
+    repo = Repo(repo_project.path)
+    if force_fetch:
+        print("Fetching remote for project ", repo_project.path, "...")
+    remote_name = 'enplug'
+
+    try:
+        repo.remotes[remote_name]           # just to check if raise error
+    except (AttributeError, IndexError):
+        remote_name = 'origin'
+
+    if force_fetch:
+        repo.remotes[remote_name].fetch()
+
+    if repo.head.is_detached:
+        return local_and_remote_are_at_same_commit(repo, remote_name, None, repo.head.commit.hexsha)
+    else:
+        local_head_name = repo.head.reference.name
+        remote = repo.remotes[remote_name]
+        if is_branch_present_on_remote(remote, local_head_name):
+            return local_and_remote_are_at_same_commit(repo, remote_name, local_head_name)
+    return False
+
+# remote compare END
+############################################################################################
+mode = "default"
+if len(sys.argv) > 1:
+    mode = str(sys.argv[1])
+
+if len(sys.argv) > 1 and not (mode in ["r", "rf"]):
+    print("Mini help:")
+    print("\t" + sys.argv[0] + " r\n\tRun script with remote mode - will compare to remotes.")
+    print("\t" + sys.argv[0] + " rf\n\tRun script with remote mode force - will fetch changes from server and compare to remote branches.")
+    sys.exit()
 
 clean_repo_paths = []
 dirty_repo_paths = []
@@ -74,17 +141,25 @@ for project in projects_list:
         clean_repo_paths.append(project.path)
 
 
-print(bcolors.GREEN + "CLEAN repos" + bcolors.ENDC)
+print(bcolors.GREEN + "\nCLEAN repos" + bcolors.ENDC)
 for repository_path in clean_repo_paths:
     print(repository_path + "/")
 
-    
+
 print(bcolors.DARK_PURPLE + "\nDIRTY repos" + bcolors.ENDC)
 for repository_path in dirty_repo_paths:
     print(repository_path + "/")
     print_untracked_files(repository_path)
     print_modified_files(repository_path)
     print_stagged_files(repository_path)
-    
 
+if mode in ["r", "rf"]:
+    print(bcolors.DARK_PURPLE + "\nRemote mode - compare local to remote" + bcolors.ENDC)
+    for project in projects_list:
+        if is_remote_and_local_same(project, mode == "rf"):
+            print(bcolors.GREEN + project.path + "/" + bcolors.ENDC)
+        else:
+            print(bcolors.RED + project.path + "/" + bcolors.ENDC)
+
+print("\n")
 
